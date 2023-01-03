939f34dd9a022db777899b2a5ce15f22652edeca
Alexandre Rulleau <alexandre.rulleau@epita.fr>
2023-01-03 02:06:13 +0100

    feat(module: vim): finish configuration
    Signed-off-by: Alexandre Rulleau <alexandre.rulleau@epita.fr>

--- assets/vim/config.lua
+++ assets/vim/config.lua
@@ -3,20 +3,14 @@
 lvim.log.level = "warn"
 lvim.colorscheme = "onedark"
 lvim.format_on_save.enabled = true
+
+local components = require("lvim.core.lualine.components")
 lvim.builtin.lualine.style = "default"
-
--- TODO: User Config for predefined plugins
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-
--- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = "all"
-lvim.builtin.treesitter.ignore_install = { "" }
-lvim.builtin.treesitter.highlight.enable = true
+lvim.builtin.lualine.sections.lualine_c = {
+    "diff",
+    components.diagnostics,
+    components.lsp
+}
 
 -- VIM options ---
 
@@ -25,9 +19,11 @@ vim.opt.guifont = "Hack Nerd Font:h17"
 vim.opt.signcolumn = "auto"
 vim.opt.updatetime = 100
 vim.opt.title = false
+vim.opt.cursorline = false
 
 vim.opt.wrap = true
 vim.opt.linebreak = true
+vim.opt.breakindent = true
 
 vim.opt.visualbell = true
 vim.opt.belloff = "all"
@@ -58,16 +54,152 @@ vim.opt.smartcase = true
 vim.opt.ignorecase = true
 vim.opt.autowrite = true
 
+-- Specific languages
+lvim.autocommands = {
+    {
+        "BufEnter",
+        {
+            pattern = { "*.py" },
+            command = "setlocal cc=120 textwidth=119",
+        }
+    },
+}
+
 --- Plugins ---
 
+-- Builtins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = "all"
+lvim.builtin.treesitter.ignore_install = { "" }
+lvim.builtin.treesitter.highlight.enable = true
+lvim.builtin.treesitter.matchup.enable = true
+lvim.builtin.treesitter.rainbow.enable = true
+
+-- Extra plugins
 lvim.plugins = {
-    { "navarasu/onedark.nvim" }
+    -- Theming
+    { "navarasu/onedark.nvim" },
+
+    -- treesitter
+    { "folke/twilight.nvim" },
+
+    -- Editing
+    { "terryma/vim-multiple-cursors" },
+    {
+        "andymass/vim-matchup",
+        event = "CursorMoved",
+        config = function()
+            vim.g.matchup_matchparen_offscreen = { method = "popup" }
+        end,
+    },
+
+    -- UI
+    { "p00f/nvim-ts-rainbow" },
+    {
+        "f-person/git-blame.nvim",
+        event = "BufRead",
+        config = function()
+            vim.cmd "highlight default link gitblame SpecialComment"
+            vim.g.gitblame_enabled = 0
+        end,
+    },
+    {
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            require("colorizer").setup({ "*" }, {
+                RGB = true, -- #RGB hex codes
+                RRGGBB = true, -- #RRGGBB hex codes
+                RRGGBBAA = true, -- #RRGGBBAA hex codes
+                rgb_fn = true, -- CSS rgb() and rgba() functions
+                hsl_fn = true, -- CSS hsl() and hsla() functions
+                css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+                css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+            })
+        end,
+    },
+
+    -- Misc
+    {
+        "nacro90/numb.nvim",
+        event = "BufRead",
+        config = function()
+            require("numb").setup {
+                show_numbers = true, -- Enable 'number' for the window while peeking
+                show_cursorline = true, -- Enable 'cursorline' for the window while peeking
+            }
+        end,
+    },
+    {
+        "tpope/vim-fugitive",
+        cmd = {
+            "G",
+            "Git",
+            "Gdiffsplit",
+            "Gread",
+            "Gwrite",
+            "Ggrep",
+            "GMove",
+            "GDelete",
+            "GBrowse",
+            "GRemove",
+            "GRename",
+            "Glgrep",
+            "Gedit"
+        },
+        ft = { "fugitive" }
+    },
+    {
+        "ray-x/lsp_signature.nvim",
+        event = "BufRead",
+        config = function() require "lsp_signature".on_attach({
+                bind = true, -- This is mandatory, otherwise border config won't get registered.
+                handler_opts = { border = "rounded" }
+            })
+        end,
+    },
+    {
+        "karb94/neoscroll.nvim",
+        event = "WinScrolled",
+        config = function()
+            require('neoscroll').setup({
+                -- All these keys will be mapped to their corresponding default scrolling animation
+                mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
+                    '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
+                hide_cursor = true, -- Hide cursor while scrolling
+                stop_eof = true, -- Stop at <EOF> when scrolling downwards
+                use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+                respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+                cursor_scrolls_alone = false, -- The cursor will keep on scrolling even if the window cannot scroll further
+            })
+        end
+    },
+    {
+        "ethanholz/nvim-lastplace",
+        event = "BufRead",
+        config = function()
+            require("nvim-lastplace").setup({
+                lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+                lastplace_ignore_filetype = {
+                    "gitcommit", "gitrebase", "svn", "hgcommit",
+                },
+                lastplace_open_folds = true,
+            })
+        end,
+    },
+    { "tpope/vim-repeat" },
+    { "tpope/vim-surround" },
+    { "felipec/vim-sanegx", event = "BufRead" },
 }
 
--- Color scheme plugin
-require('onedark').setup {
-    style = 'deep'
-}
+-- Onedark theming
+require('onedark').setup { style = 'deep' }
 require('onedark').load()
 
 --- Key Mappings ---

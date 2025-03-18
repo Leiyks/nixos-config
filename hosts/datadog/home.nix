6f903b3e7ea47ead69ed5502feb4bcdcadf25dbb
Alexandre Rulleau <alexandre.rulleau@datadoghq.com>
2025-03-18 16:28:42 +0100

    feat(hosts): add new host profile
    Signed-off-by: Alexandre Rulleau <alexandre.rulleau@datadoghq.com>

--- hosts/datadog/home.nix
+++ hosts/datadog/home.nix
@@ -4,11 +4,18 @@
   # Specific packages for WSL
   home = {
     packages = with pkgs; [
-      glib.out
-      zlib
+      rustup
     ];
 
     sessionPath = [
+
     ];
   };
+
+  programs.zsh.initExtra = ''
+      # printf '\eP$f{"hook": "SourcedRcFileForWarp", "value": { "shell": "zsh"}}\x9c' && clear
+      eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+      umask u=rwx,g=rwx,o=rwx
+    '';
 }
+
